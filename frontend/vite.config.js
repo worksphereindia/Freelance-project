fabf75d09ba710dbf31af428c39348e6cf6bd2fa
Harikiran <harikiranap123@gmail.com>
2026-08-18 21:39:40 +0530

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups"
+    }
+  },
+  preview: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups"
+    }
+  },
   build: {
     chunkSizeWarningLimit: 1500,
   },
